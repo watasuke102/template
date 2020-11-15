9cc26afb81037e812fb5facce0430dcebb38e8c6
watasuke102 <watasuke102@gmail.com>
2020-11-16 07:11:12 +0900

    update: 追加ミスしていたのでやりなおし

---
+++ DxLib/README.md
@@ -0,0 +1,11 @@
+# dxlib-template
+DXライブラリをMinGWで使う際のテンプレート
+
+## depends
+- MinGW
+- CMake
+
+## How-to-use
+1. DXライブラリ Windows版 MinGW用をダウンロードして展開
+1. 「プロジェクトに追加すべきファイル_GCC(MinGW)用」内の、名前が最もMinGWのバージョンに近いフォルダの中身を全てlibフォルダにコピーする
+1. cmakeする
---
+++ DxLib/src/main.cpp
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    if( DxLib_Init() == -1 ) return -1;
+
+    DrawPixel(320, 240, GetColor(255, 255, 255));
+
+    WaitKey();
+
+    DxLib_End();
+    return 0;
+}
